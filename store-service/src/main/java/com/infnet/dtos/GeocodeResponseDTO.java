8941c6d1a8e5ca6d0753cb222d3b6931cfc77360
Lucas Gomes De Araujo <lucas.araujo@g.globo>
2026-06-22 18:49:08 -0300

    feat: refactor order dtos

--- store-service/src/main/java/com/infnet/dtos/GeocodeResponseDTO.java
+++ store-service/src/main/java/com/infnet/dtos/GeocodeResponseDTO.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class GeocodeResponseDTO {
-    private Double latitude;
-    private Double longitude;
+    private Double lat;
+    private Double lon;
 }
